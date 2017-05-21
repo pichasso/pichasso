61974402108a3e069a8619be59e2ee3fbc5fd77a
Felix Musmann <fmusmann@gmail.com>
2017-05-21 16:27:21 +0200

    resize image

--- routes/image.js
+++ routes/image.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const imageLoader = require('../middleware/imageLoader');
+const sharp = require('sharp');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.status(501).send('This Route is not yet implemented.');
+router.get('/', imageLoader, function(req, res, next) {
+  const width = Number(req.query.width);
+  const height = Number(req.query.height);
+
+  sharp(req.image)
+    .resize(width, height)
+    .jpeg()
+    .toBuffer()
+    .then((buffer) => {
+      res.write(buffer, 'binary');
+      res.end(null, 'binary');
+    })
+    .catch((error) => {
+      res.status(500).render('error', {error: error});
+    });
 });
 
 router.get('/test', function(req, res, next){
