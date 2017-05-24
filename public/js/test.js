a19a16ae3f4c5bfab6682e4f1aa2b5501b8385a3
Tobias Wollowski <tobias@jbbr.net>
2017-05-24 10:59:11 +0200

    show test result in page

--- public/js/test.js
+++ public/js/test.js
@@ -1,5 +1,23 @@
 $('#compress').slider({
-  formatter: function(value) {
+  formatter: function (value) {
     return 'Current value: ' + value;
   }
+});
+
+function updateImageUrl() {
+  var values = $('#configure-form').serialize();
+  console.log(values);
+  $('#result-image').attr('src', '/image?' + values);
+}
+
+$(document).ready(function(){
+  $('#configure-form').submit(function (e) {
+    e.preventDefault();
+    updateImageUrl();
+  });
+
+  $('#update-button').click(function (e) {
+    e.preventDefault();
+    updateImageUrl();
+  });
 });
