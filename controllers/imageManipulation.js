4b4ed7cba8a1fc58ddb4b20ad24be414d42f1380
Felix Musmann <fmusmann@gmail.com>
2017-05-24 12:15:47 +0200

    add fill, fit, scale cropping

--- controllers/imageManipulation.js
+++ controllers/imageManipulation.js
@@ -1,4 +1,5 @@
 const sharp = require('sharp');
+const probe = require('probe-image-size');
 
 function resize(req, res, next) {
   if (!req.query.width && !req.query.height) {
@@ -7,11 +8,53 @@ function resize(req, res, next) {
 
   const width = Number(req.query.width);
   const height = Number(req.query.height);
+  const aspectRatio = width / height;
+  const crop = req.query.crop || 'fill';
+  const gravity = req.query.gravity ? sharp.gravity[req.query.gravity] : sharp.gravity.center;
 
-  sharp(req.image)
-    .resize(width, height)
-    .max()
-    .toBuffer()
+  let sharpInstance = sharp(req.image);
+
+  switch (crop) {
+    case 'fill': {
+      const imgProperties = probe.sync(req.image);
+      const imgAspectRatio = imgProperties.width / imgProperties.height;
+      let fillWidth, fillHeight;
+      if (imgAspectRatio >= aspectRatio) {
+        fillWidth = Math.ceil(aspectRatio * height);
+        fillHeight = height;
+      } else {
+        fillWidth = width;
+        fillHeight = Math.ceil(width / imgAspectRatio);
+      }
+      sharpInstance
+        .resize(fillWidth, fillHeight)
+        .max()
+        .resize(width, height)
+        .crop(gravity);
+      break;
+    }
+    case 'fit': {
+      sharpInstance
+        .resize(width, height)
+        .max();
+      break;
+    }
+    case 'scale': {
+      sharpInstance
+        .resize(width, height)
+        .ignoreAspectRatio();
+      break;
+    }
+    default: {
+      res.render(
+        'error',
+        {error: new Error(`invalid cropping method ${crop}`)}
+      );
+      return;
+    }
+  }
+
+  sharpInstance.toBuffer()
     .then((buffer) => {
       req.image = buffer;
       next();
