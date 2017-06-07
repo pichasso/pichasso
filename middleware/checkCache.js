306df4d3bd0bb94401f768bd0251bb0b76a38bc0
Tobias Wollowski <tobias@jbbr.net>
2017-06-07 15:17:46 +0200

    remove logs

--- middleware/checkCache.js
+++ middleware/checkCache.js
@@ -3,15 +3,13 @@ const hash = require('object-hash');
 const probe = require('probe-image-size');
 
 function checkCache(req, res, next) {
-  console.log('check cache');
-  // generate hash and check if image exists
+  // generate hash and check if image exists in cache
   let accept = req.get('accept');
   if (!req.query.format && /image\/webp/.test(accept)) {
     req.query.format = 'webp';
   }
 
   let queryHash = hash(req.query);
-  console.log('check cache for', req.query, queryHash);
 
   if (cache.exists(queryHash)) {
     req.image = cache.load(queryHash);
