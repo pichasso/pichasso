d5344e1cf7d73a6e3017778aec7dfc38dc953b3f
Felix Musmann <fmusmann@gmail.com>
2017-06-10 15:07:02 +0200

    test for etag consistency and file persistence

--- test/cache.js
+++ test/cache.js
@@ -1,12 +1,43 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const config = require('config');
+const fs = require('fs');
 const server = require('../app.js');
 
 chai.should();
 chai.use(chaiHttp);
 
 describe('Cache', () => {
-  it('should return cached image for etag', (done) => {
+  it('should cache the requested image', (done) => {
+    chai.request(server)
+      .get('/image?url=https://http.cat/400')
+      .end((err, res) => {
+        res.status.should.equal(200);
+        const filePath = config.get('Caching.Imagepath') + res.headers.etag;
+        fs.exists(filePath, (exists) => {
+          exists.should.be.true;
+          done();
+        });
+      });
+  });
+
+  it('should return same etag for same image', (done) => {
+    chai.request(server)
+      .get('/image?url=https://http.cat/400')
+      .end((err, res) => {
+        res.status.should.equal(200);
+        const etag = res.headers.etag;
+        chai.request(server)
+          .get('/image?url=https://http.cat/400')
+          .end((err, res) => {
+            res.status.should.equal(200);
+            etag.should.equal(res.headers.etag);
+            done();
+          });
+      });
+  });
+
+  it('should return status 304 for etag', (done) => {
     chai.request(server)
       .get('/image?url=https://http.cat/400')
       .end((err, res) => {
