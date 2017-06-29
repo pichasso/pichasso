9d61debd8ea3298c52f4fbb511719c634be0fd73
Tobias Wollowski <tobias@jbbr.net>
2017-06-29 13:01:02 +0200

    beautify

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = new express.Router();
-const error = require('http-errors');
 const onlyDevelopment = require('../middleware/onlyDevelopment');
 
 /* GET home page. */
-router.get('/', onlyDevelopment, function (req, res, next) {
-    res.render('index', {title: 'Pichasso'});
- });
+router.get('/', onlyDevelopment, function (req, res) {
+  res.render('index', {title: 'Pichasso'});
+});
 
 module.exports = router;
