4e36563c4553ca2748ec4d5e461c12e966440ba8
Tobias Wollowski <tobias@jbbr.net>
2017-06-29 17:49:25 +0200

    cleanup

--- routes/pdf.js
+++ routes/pdf.js
@@ -8,7 +8,7 @@ const fileLoader = require('../middleware/fileLoader');
 router.get('/:file/:quality*?', fileLoader, (req, res) => {
   // res.setHeader('Cache-Control', 'public, max-age=' + config.get('Caching.Expires'));
   // res.setHeader('Expires', new Date(Date.now() + config.get('Caching.Expires')).toUTCString());
-  //res.setHeader('Content-Type', 'application/pdf');
+  res.setHeader('Content-Type', 'application/pdf');
   // res.render('pdf', { params: req.params });
   res.end(req.compressedFile, 'binary');
 });
