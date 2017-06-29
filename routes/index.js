ef7040c5f2a1d505f8539cc882df0a61c3e58796
Tobias Wollowski <tobias@jbbr.net>
2017-06-29 10:45:34 +0200

    added dev only middleware

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const router = new express.Router();
 const error = require('http-errors');
+const onlyDevelopment = require('../middleware/onlyDevelopment');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  if (req.app.get('env') === 'development') {
+router.get('/', onlyDevelopment, function (req, res, next) {
     res.render('index', {title: 'Pichasso'});
-  } else {
-    return next(new error.Forbidden());
-  }
-});
+ });
 
 module.exports = router;
