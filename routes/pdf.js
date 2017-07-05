469d8e7fa1fdd54c91eb13c159e5f75831657e6e
Tobias Wollowski <tobias@jbbr.net>
2017-07-05 11:12:06 +0200

    correct headers for pdf file response

--- routes/pdf.js
+++ routes/pdf.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const router = new express.Router();
-// const config = require('config');
+const config = require('config');
 const fileLoader = require('../middleware/fileLoader');
-// const error = require('http-errors');
+const loadParamsFromQuery = require('../middleware/loadParamsFromQuery');
 
 /* GET pdf. */
-router.get('/:file/:quality*?', fileLoader, (req, res) => {
-  // res.setHeader('Cache-Control', 'public, max-age=' + config.get('Caching.Expires'));
-  // res.setHeader('Expires', new Date(Date.now() + config.get('Caching.Expires')).toUTCString());
-  res.setHeader('Content-Type', 'application/pdf');
-  // res.render('pdf', { params: req.params });
+router.get('/', loadParamsFromQuery, fileLoader, (req, res) => {
+  res.setHeader('Cache-Control', 'public, max-age=' + config.get('Caching.Expires'));
+  res.setHeader('Expires', new Date(Date.now() + config.get('Caching.Expires')).toUTCString());
+  if (req.get('Content-Type')) {
+    res.setHeader('Content-Type', req.get('Content-Type') || 'application/pdf');
+  } else {
+    res.setHeader('Content-Type', 'application/pdf');
+  }
+  if (req.get('Content-Disposition')) {
+    res.setHeader('Content-Disposition', req.get('Content-Disposition'));
+  } else {
+    let filepath = req.params.file.split('/');
+    if (filepath.length) {
+      let filename = filepath[filepath.length - 1];
+      res.setHeader('Content-Disposition', `inline; filename="${filename}"`);
+    }
+  }
   res.end(req.compressedFile, 'binary');
 });
 
