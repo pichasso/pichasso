29dae06b9b3a457831421b97ddf08990af04a396
Felix Musmann <fmusmann@gmail.com>
2017-07-05 11:25:59 +0200

    log inside app dir

--- controllers/logger.js
+++ controllers/logger.js
@@ -1,9 +1,10 @@
 const config = require('config');
+const DailyRotateFile = require('winston-daily-rotate-file');
 const fs = require('fs');
 const winston = require('winston');
 
-const env = process.env.NODE_ENV || 'development';
 const dir = config.get('Logging.Directory');
+const env = process.env.NODE_ENV || 'development';
 const tsFormat = () => (new Date()).toLocaleTimeString();
 
 if (!fs.existsSync(dir)) {
@@ -17,7 +18,7 @@ const logger = new (winston.Logger)({
       colorize: true,
       level: env === 'development' ? 'verbose' : 'none',
     }),
-    new (require('winston-daily-rotate-file'))({
+    new (DailyRotateFile)({
       filename: `${dir}-pichasso.log`,
       timestamp: tsFormat,
       datePattern: 'yyyy-MM-dd',
