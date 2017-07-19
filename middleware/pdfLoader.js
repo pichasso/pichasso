f4f68597b8be94a1ba4972307a3b8beeac1d7e24
Felix Musmann <fmusmann@gmail.com>
2017-07-19 10:57:14 +0200

    move extractFilename to own module

--- middleware/pdfLoader.js
+++ middleware/pdfLoader.js
@@ -2,6 +2,7 @@ const config = require('config');
 const error = require('http-errors');
 const request = require('request');
 const PDFCompressor = require('./pdfCompressor');
+const extractFilename = require('./extractFilename');
 
 function pdfLoader(req, res, next) {
   if (req.completed) {
@@ -54,29 +55,6 @@ function pdfLoader(req, res, next) {
       return next(new error.BadRequest(`File exceeds size limit of ${sizeLimit} KB.`));
     }
   });
-}
-
-function extractFilename(response, fileParam) {
-  const filenameRegExp = /filename=\"(.+)\"/ig;
-  const contentDisposition = response.headers['content-disposition'];
-  if (contentDisposition && contentDisposition.search(filenameRegExp)) {
-    return filenameRegExp.exec(contentDisposition)[1];
-  }
-
-  while (fileParam.endsWith('/')) {
-    fileParam = fileParam.substr(0, fileParam.length - 1);
-  }
-
-  let filename = '';
-  const delimiterIndex = fileParam.lastIndexOf('/');
-  if (delimiterIndex !== -1) {
-    filename = fileParam.substr(delimiterIndex + 1);
-  }
-  if (!filename.endsWith('.pdf')) {
-    filename += '.pdf';
-  }
-
-  return filename;
 }
 
 module.exports = pdfLoader;
