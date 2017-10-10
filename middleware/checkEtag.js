027f2ec9b50e22aa55a5fd09884201d01e272f9d
Tobias Wollowski <tobias@jbbr.net>
2017-10-10 17:00:09 +0200

    quote etag hash

--- middleware/checkEtag.js
+++ middleware/checkEtag.js
@@ -6,7 +6,7 @@ function checkEtag(req, res, next) {
   let etag = req.get('If-None-Match');
   if (etag && cache.valid(etag)) {
     logger.verbose(logTag, 'Respond with etag');
-    res.set('Etag', etag);
+    res.set('Etag', '"' + etag + '"');
     res.status(304);
     res.end();
   } else {
